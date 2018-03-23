c3b1f86fd4a5404285245936dea893e03a292766
Jay Salvat <jay@jaysalvat.com>
2018-03-23 19:07:18 +0100

    Update twig markdown function to accept text and files

--- src/views/_mixins/markdown.js
+++ src/views/_mixins/markdown.js
@@ -2,7 +2,17 @@ const fs   = require('fs');
 const marked = require('marked');
 
 const markdown = function (value) {
-  let markdown = fs.readFileSync('./src/markdown/' + value, 'utf-8');
+  let markdown;
+  const pathfile = './src/markdown/' + value;
+
+  if (fs.existsSync(pathfile)) {
+    markdown = fs.readFileSync(pathfile, 'utf-8');
+  } else {
+    markdown = value;
+
+    markdown = markdown.replace(/ {1,}/g, ' ');
+  }
+
   let html = marked(markdown);
 
   return html;
