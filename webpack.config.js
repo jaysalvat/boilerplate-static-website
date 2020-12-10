f31c3f75969c9d715269ab8ac303d5710a37396a
Jay Salvat <jay@jaysalvat.com>
2020-12-10 17:13:34 +0100

    Remove Webpack splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,7 @@ module.exports = (argv) => {
     devtool: argv.production ? false : 'source-map',
     mode: argv.production ? 'production' : 'development',
     optimization: {
-      minimizer: [],
-      splitChunks: {
-        chunks: 'async',
-        minSize: 20000,
-        minRemainingSize: 0,
-        maxSize: 100000,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        automaticNameDelimiter: '~',
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true
-          }
-        }
-      }
+      minimizer: []
     },
     output: {
       filename: '[name].bundle.js',
